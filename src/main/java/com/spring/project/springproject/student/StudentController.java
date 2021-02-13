222830f31ffeef2626c4ac86f5e2483d6d605672
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2021-02-13 18:16:01 +0530

    Added dependency injection

--- src/main/java/com/spring/project/springproject/student/StudentController.java
+++ src/main/java/com/spring/project/springproject/student/StudentController.java
@@ -1,8 +1,8 @@
 package com.spring.project.springproject.student;
 
-import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+	private final StudentService studentService;
+	
+	@Autowired
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
 	@GetMapping
 	public List<Student> getStudent() {
-		return List.of(new Student(1L, "Subhasish", "subhasish064@gmail.com", LocalDate.of(1991, 05, 25), 29));
+		return studentService.getStudent();
 	}
 
 }
---
+++ src/main/java/com/spring/project/springproject/student/StudentService.java
@@ -0,0 +1,15 @@
+package com.spring.project.springproject.student;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentService {
+
+	public List<Student> getStudent() {
+		return List.of(new Student(1L, "Subhasish", "subhasish064@gmail.com", LocalDate.of(1991, 05, 25), 29));
+	}
+
+}
